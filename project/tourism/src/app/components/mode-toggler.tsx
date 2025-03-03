158266acc889bd4879172a41e9e8cb316456ccdd
mukesh <mukeshnarravula14@gmail.com>
2025-03-03 17:16:04 +0530

    Taken care of the dark theme

--- project/tourism/src/app/components/mode-toggler.tsx
+++ project/tourism/src/app/components/mode-toggler.tsx
@@ -12,19 +12,19 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {theme === "light" ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
+          {theme === "light" ? <Sun className="h-5 w-5 text-yellow-500" /> : <Moon className="h-5 w-5 text-gray-300" />}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-4 w-4" /> Light
+      <DropdownMenuContent align="end" className="dark:bg-gray-800 dark:text-white">
+        <DropdownMenuItem onClick={() => setTheme("light")} className="dark:hover:bg-gray-700 dark:hover:text-yellow-400">
+          <Sun className="mr-2 h-4 w-4 text-yellow-500" /> Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4" /> Dark
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="dark:hover:bg-gray-700 dark:hover:text-gray-300">
+          <Moon className="mr-2 h-4 w-4 text-gray-300" /> Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Laptop className="mr-2 h-4 w-4" /> System
+        <DropdownMenuItem onClick={() => setTheme("system")} className="dark:hover:bg-gray-700 dark:hover:text-blue-400">
+          <Laptop className="mr-2 h-4 w-4 text-blue-500" /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
